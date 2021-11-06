12eab03c763ec1475ad782c2f6e7507af2ccdb99
Nipun Chamikara <theharmlesscreeper2002@gmail.com>
2021-11-06 18:18:27 +0530

    Added hamburger menu to headers

--- src/components/layout/auth-header/AuthHeader.js
+++ src/components/layout/auth-header/AuthHeader.js
@@ -5,13 +5,34 @@ export class AuthHeader {
     this.element = document.createElement("header");
     this.element.id = "auth-header";
     this.element.className = "auth-header";
+
+    const div = document.createElement("div");
+    div.className = "auth-header-main";
+
     const p = document.createElement("p");
     p.className = "auth-header-brand";
     p.innerHTML = "N<sup>2</sup>IPD";
     p.addEventListener("click", () => {
       router.currentPath = "/auth";
     });
+
+    div.appendChild(p);
+
     const ul = document.createElement("ul");
+
+    const ham = document.createElement("a");
+    ham.className = "auth-header-ham";
+    ham.addEventListener("click", () => {
+      if (this.element.classList.contains("visible")) {
+        this.element.classList.remove("visible");
+      } else {
+        this.element.classList.add("visible");
+      }
+    });
+    const span = document.createElement("span");
+    ham.appendChild(span);
+    div.appendChild(ham);
+
     ul.className = "auth-header-navigation-list";
     ul.appendChild(
       (function () {
@@ -46,7 +67,7 @@ export class AuthHeader {
         return li;
       })()
     );
-    this.element.appendChild(p);
+    this.element.appendChild(div);
     this.element.appendChild(ul);
     return this.element;
   }
